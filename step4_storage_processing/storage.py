19e025843bdf6a06fd3c0060013811ebf1f0d500
billalerat <bbillal93380@gmail.com>
2026-02-14 16:15:56 +0100

    Create storage.py for MongoDB storage class for FHIR observations

--- step4_storage_processing/storage.py
+++ step4_storage_processing/storage.py
@@ -1,24 +1,18 @@
-import pymongo
-
 class MongoDBStorage:
-    def __init__(self, uri, database):
-        self.client = pymongo.MongoClient(uri)
-        self.db = self.client[database]
+    def __init__(self, db_name, collection_name):
+        from pymongo import MongoClient
+        self.client = MongoClient()
+        self.db = self.client[db_name]
+        self.collection = self.db[collection_name]
 
-    def store_observation(self, observation):
-        """Store an observation in the MongoDB database."""
-        return self.db.observations.insert_one(observation)
+    def insert_observation(self, observation):
+        self.collection.insert_one(observation)
 
-    def retrieve_observation(self, observation_id):
-        """Retrieve an observation by its ID."""
-        return self.db.observations.find_one({'_id': observation_id})
+    def find_observation(self, observation_id):
+        return self.collection.find_one({'id': observation_id})
 
-    def aggregate_observations(self, pipeline):
-        """Aggregate observations based on the provided pipeline."""
-        return list(self.db.observations.aggregate(pipeline))
+    def update_observation(self, observation_id, updated_data):
+        self.collection.update_one({'id': observation_id}, {'$set': updated_data})
 
-# Example usage:
-# storage = MongoDBStorage('mongodb://localhost:27017/', 'fhir_database')
-# storage.store_observation({'name': 'Blood Pressure', 'value': 120})
-# observation = storage.retrieve_observation(some_id)
-# aggregation_result = storage.aggregate_observations([{ '$group': { '_id': '$name', 'avg_value': { '$avg': '$value' } }}])
+    def delete_observation(self, observation_id):
+        self.collection.delete_one({'id': observation_id})
